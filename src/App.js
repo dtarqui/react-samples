9642547df48b1dde1595e6378e8bd56e50eceb5f
Fulano2141 <macuto2142dt@gmail.com>
2021-03-07 22:13:33 -0400

    states, methods and components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from "./Person/Person.js"
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Cosme', age:23},
+      { name: 'Fulano', age:24},
+      { name: 'Fhulano', age:25},
+    ],
+    otherState: 'some value'
+  }
+
+  switchNameHandler = () => {
+    console.log(this.state)
+    // DON'T DO THIS this.state.persons[0].name = 'Fulanero'
+    this.setState({
+      persons: [
+        { name: 'Fulanero', age:23},
+        { name: 'Fulano', age:24},
+      ],
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React App</h1>
+        <p>Work</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name='Cosme' age='23' />
+        <Person name='Fulano' age='24'>My Hobbies: Racing </Person>
+        <Person name='Fhulano' age='25' />
+        {/* <Person name={this.state}></Person> */}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement())
   }
 }
 
