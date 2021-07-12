596380bc88136a2c25914d4e121e4901ef281cb4
macut <macuto2142dt@gmail.com>
2021-07-12 14:20:59 -0400

    react basics

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const expenseDate = props.date;
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={expenseDate} />
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
